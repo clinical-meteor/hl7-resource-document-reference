61ad4e8a0142c1ee0d16e056256091f5bc7cfef7
Abigail Watson <abigailwatson@Abigails-Mac-mini-380428.local>
2018-12-17 04:57:17 -0600

    modern syntax

--- tests/DocumentReferenceTests.tests.js
+++ tests/DocumentReferenceTests.tests.js
@@ -1,17 +1,31 @@
-describe('clinical:hl7-resources-document-references', function () {
-  var server = meteor();
-  var client = browser(server);
+// describe('clinical:hl7-resources-document-references', function () {
+//   var server = meteor();
+//   var client = browser(server);
 
-  it('DocumentReferences should exist on the client', function () {
-    return client.execute(function () {
-      expect(DocumentReferences).to.exist;
-    });
+//   it('DocumentReferences should exist on the client', function () {
+//     return client.execute(function () {
+//       expect(DocumentReferences).to.exist;
+//     });
+//   });
+
+//   it('DocumentReferences should exist on the server', function () {
+//     return server.execute(function () {
+//       expect(DocumentReferences).to.exist;
+//     });
+//   });
+
+// });
+
+
+
+describe('clinical:hl7-resource-document-reference', function () {
+  beforeEach(function () {
+    //console.log('beforeEach');
   });
-
-  it('DocumentReferences should exist on the server', function () {
-    return server.execute(function () {
-      expect(DocumentReferences).to.exist;
-    });
+  afterEach(function () {
+    //console.log('afterEach');
   });
-
+  it('exists globally', function () {
+    expect(DocumentReferences).to.exist;
+  });
 });
